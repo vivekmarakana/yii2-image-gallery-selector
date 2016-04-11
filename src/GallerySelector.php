10b2acff6b668b0b2c7686c668cb068306a9a613
Vivek Marakana <vivek.marakana@gmail.com>
2016-04-11 12:08:00 +0530

    gpg sign test

--- src/GallerySelector.php
+++ src/GallerySelector.php
@@ -2,15 +2,13 @@
 
 namespace vivekmarakana\widgets;
 
-class GallerySelector extends \yii\base\Widget
-{
+class GallerySelector extends \yii\base\Widget {
     const WIDGET_NAME = 'gallery-selector';
 
     public $images = [];
     public $id;
 
-    public function run()
-    {
+    public function run() {
         return $this->render('gallery-selector', [
             'images' => $this->images,
             'id' => (!empty($this->id)) ? $this->id : 'timeline-widget-' . $this->getId()
